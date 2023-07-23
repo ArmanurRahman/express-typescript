6ecba39778df27bceee8a0f11513e440bcbc3cc6
Armanur Rahman <mubeenarman19@gmail.com>
2023-07-23 20:54:10 +0900

    add use decorator

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from "express";
-import { get } from "./decorators/routes";
-import { controller } from "./decorators/controller";
+import { NextFunction, Request, Response } from "express";
+import { controller, get, use } from "./decorators";
 
+function logger(req: Request, res: Response, next: NextFunction) {
+    console.log("Request has made");
+    next();
+}
 @controller("/auth")
 class LoginController {
     @get("/login")
+    @use(logger)
     getLogin(req: Request, res: Response): void {
         res.send(`
         <form method="POST">
