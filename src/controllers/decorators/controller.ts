692a407ddc609397d274ec6fb556d608ea9ff1c8
Armanur Rahman <mubeenarman19@gmail.com>
2023-07-23 20:25:10 +0900

    add support for all http method in controller

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
-import express from "express";
 import { AppRouter } from "../../AppRouter";
+import { Methods } from "./Methods";
 
 export const router = AppRouter.gerInstance();
 
@@ -9,9 +9,14 @@ export function controller(routePrefix: string) {
         for (let key in target.prototype) {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata("path", target.prototype, key);
+            const method: Methods = Reflect.getMetadata(
+                "method",
+                target.prototype,
+                key
+            );
 
             if (path) {
-                router.get(`${routePrefix}${path}`, routeHandler);
+                router[method](`${routePrefix}${path}`, routeHandler);
             }
         }
     };
